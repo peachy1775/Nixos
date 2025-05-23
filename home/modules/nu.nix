5f04f53a9380a5fe40a423f0fc2b540d56d4639c
Dont_Matter <204289133+peachy1775@users.noreply.github.com>
2025-05-22 21:50:37 -0500

    .

--- home/modules/nu.nix
+++ home/modules/nu.nix
@@ -1,9 +1,64 @@
 {...}: {
 
-    programs = {
+
+  programs = {
+
+    #zed-editor.enable = true;
+   
       nushell = {
         enable = true;
-        configFile.source = ./config/config.nu;
+        configFile.source = ../config/config.nu;
+      };
+    
+
+    starship = {
+      enable = true;
+      enableNushellIntegration = true;
+    };
+
+    carapace = {
+      enable = true;
+      enableNushellIntegration = true;
+    };
+
+    zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+      enableNushellIntegration = true;
+      options = [ "--cmd cd" ];
+    };
+
+    nixcord = {
+      enable = true;
+      discord.enable = false;
+      vesktop.enable = true;
+      quickCss = builtins.readFile ../config/vesktop/main.css;
+      config = {
+        useQuickCss = true;
+        themeLinks = [ ];
+        frameless = true;
+        plugins = {
+          betterSettings.enable = true;
+          callTimer.enable = true;
+          crashHandler.enable = true;
+          fixYoutubeEmbeds.enable = true;
+          imageZoom.enable = true;
+          noF1.enable = true;
+          onePingPerDM.enable = true;
+          openInApp.enable = true;
+          quickReply.enable = true;
+          voiceChatDoubleClick.enable = true;
+          voiceDownload.enable = true;
+          voiceMessages.enable = true;
+          volumeBooster = {
+            enable = true;
+            multiplier = 5;
+          };
+          webKeybinds.enable = true;
+          webScreenShareFixes.enable = true;
+          youtubeAdblock.enable = true;
+        };
       };
     };
-}
+  };
+}    
