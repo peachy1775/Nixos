8f2c3967abfedc7b110f03ed6fc109ebd0d5f024
peachy1775 <204289133+peachy1775@users.noreply.github.com>
2025-05-24 20:20:04 -0500

    yazi

--- home/modules/files.nix
+++ home/modules/files.nix
@@ -1,19 +1,26 @@
 { ... }:
 {
-  home = {
-    file = {
-      ".config/starship.toml" = {
-        source = ../config/starship.toml;
-      };
-      ".config/fastfetch/nixos.png" = {
-        source = ../config/fastfetch/nixos.png;
-      };
-      ".config/waybar/config.jsonc" = {
-        source = ../config/waybar/config.json;
-      };
-      ".config/waybar/style.css" = {
-        source = ../config/waybar/style.css;
-      };
+  home.file = {
+    ".config/yazi/yazi.toml" = {
+      source = ../config/yazi/yazi.toml;
+    };
+    ".config/yazi/keymap.toml" = {
+      source = ../config/yazi/keymap.toml;
+    };
+    ".config/yazi/theme.toml" = {
+      source = ../config/yazi/theme.toml;
+    };
+    ".config/starship.toml" = {
+      source = ../config/starship.toml;
+    };
+    ".config/fastfetch/nixos.png" = {
+      source = ../config/fastfetch/nixos.png;
+    };
+    ".config/waybar/config.jsonc" = {
+      source = ../config/waybar/config.json;
+    };
+    ".config/waybar/style.css" = {
+      source = ../config/waybar/style.css;
     };
   };
 }
