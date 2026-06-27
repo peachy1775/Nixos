a19a3d54eba843a3f34a1c76544ef835cc0eb52c
peachy1775 <204289133+peachy1775@users.noreply.github.com>
2026-06-26 19:54:31 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     stylix.url = "github:danth/stylix";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     nixcord.url = "github:kaylorben/nixcord";
     treefmt-nix.url = "github:numtide/treefmt-nix";
 
