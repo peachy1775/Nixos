92d71b2e25eb461cc9f9d8abb5a9afce35d611af
peachy1775 <204289133+peachy1775@users.noreply.github.com>
2026-07-02 16:20:52 -0500

    Update hyprland.nix

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -1,5 +1,10 @@
-_: {
+{ ... }:
+
+{
   wayland.windowManager.hyprland.enable = true;
 
   home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  xdg.configFile."hypr/hyprland.lua".source =
+    ./hyprland.lua;
 }
