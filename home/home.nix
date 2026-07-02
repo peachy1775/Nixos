d515b12a6044b76c18cbd5a70d0020e888ee56b8
peachy1775 <204289133+peachy1775@users.noreply.github.com>
2026-07-02 16:21:16 -0500

    Update home.nix

--- home/home.nix
+++ home/home.nix
@@ -11,19 +11,20 @@
     ./modules/programs.nix
     ./modules/files.nix
   ];
-xdg.configFile."hypr/hyprland.lua".source = ./hyprland.lua;
+   xdg.configFile."hypr/hyprland.lua".source = ./modules/hyprland.lua;
+
   home = {
     inherit username;
     homeDirectory = "/home/peaches";
-    stateVersion = "24.11";
+    stateVersion = "26.05";
 
     sessionVariables = {
       EDITOR = "nvim";
       GIT_EDITOR = "nvim";
 
-      XDG_CURRENT_DESKTOP = "Hyprland";
-      XDG_SESSION_TYPE = "wayland";
-      XDG_SESSION_DESKTOP = "Hyprland";
+      XDG_CURRENT_DESKTOP = "hyprland";
+      XDG_SESSION_TYPE = "hyprland";
+      XDG_SESSION_DESKTOP = "KDE";
       GTK_USE_PORTAL = "1";
 
     };
